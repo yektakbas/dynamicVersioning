ca788659679279ef17431095c60004d908459c4d
aliggulec <aliggulec@gmail.com>
2023-09-28 10:48:13 +0300

    dynamicVersioning.py imtag loop recovered

--- docker.py
+++ docker.py
@@ -30,7 +30,7 @@ def docker_rm():
 def docker_load():
     '''Load new  docker images to the local docker prepository'''
 
-    directory_path = "/root/0.9.14-botas-scada"
+    directory_path = "/root/kassg_image"
 
     # Get a list of .tar files in the directory
     image_files = [file for file in os.listdir(directory_path) if file.endswith(".tar")]
@@ -65,40 +65,10 @@ def image_file_create():
             print("No Docker images found.")
         else:
             # Write image names and tags to a text file
-            with open("images.txt", "w") as file:
+            with open("docker_images.txt", "w") as file:
                 file.write("\n".join(image_info))
 
             print("Docker images written to docker_images.txt successfully.")
 
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
-
-
-def image_push():
-
-    # Define the filename containing the list of Docker images to push
-    image_list_file = "versioned_image_file.txt"  # Replace with your file name
-
-    try:
-        # Open the file for reading
-        with open(image_list_file, "r") as file:
-            # Iterate through each line in the file
-            for line in file:
-                # Strip leading/trailing whitespace from the line
-                image_name = line.strip()
-
-                # Define the Docker push command for the current image
-                push_command = ["docker", "push", image_name]
-
-                try:
-                    # Run the Docker push command for the current image
-                    subprocess.run(push_command, capture_output=True, text=True, check=True)
-                    print(f"Pushed Docker image: {image_name}")
-
-                except subprocess.CalledProcessError as e:
-                    print(f"Error pushing Docker image {image_name}: {e}")
-
-    except FileNotFoundError:
-        print(f"The file '{image_list_file}' was not found.")
-    except IOError as e:
-        print(f"An error occurred while reading the file: {e}")
--- dynamicVersioning.py
+++ dynamicVersioning.py
@@ -1,11 +1,9 @@
 import subprocess
 import os
 
-
 def image_versioning():
     # Define the filenames and variables
     docker_file_name = "docker_images.txt"  # Replace with the actual file name
-    version = "version.txt"
     versioned_image_file = "versioned_image_file.txt"
 
     try:
@@ -30,7 +28,8 @@ def image_versioning():
                 # Append the extracted service name to the list
                 extracted_parts.append(service_name)
 
-        with open(version, "r") as vr:
+
+        with open("version.txt", "r") as vr:
             # Read the content of the version.txt file
             version_content = vr.read().strip()
 
@@ -38,38 +37,38 @@ def image_versioning():
             if version_content:
                 # Convert version_content to float and increment by 0.1
                 tag = float(version_content) + 0.1
-                formatted_tag = "{:.2f}".format(tag)
+                formatted_tag = "{:.1f}".format(tag)
             else:
                 # Set a default value if version.txt is empty or not a valid float
                 tag = 1.0
-                formatted_tag = "{:.2f}".format(tag)
+                formatted_tag = "{:.1f}".format(tag)
 
-        with open(version, "w") as vr:
+        with open("version.txt", "w") as vr:
             vr.write(str(formatted_tag))
 
         # Open the versioned_image_file.txt for writing
-            with open(versioned_image_file, "w") as vf:
-                # Write versioned_image values to the file line by line
-                print("Writing Versioned Images to versioned_image_file.txt:")
-                for part in extracted_parts:
-                    image_name = part
-                    image_tag = formatted_tag
-                    versioned_image = f'harbor1.akillisebeke.lab/scada-botas/{image_name}:{image_tag}'
-                    tag_image_command = ["docker", "tag", line, versioned_image]
-                    vf.write(versioned_image + "\n")
-                    print(versioned_image)  # Print for verification
+        with open(versioned_image_file, "w") as vf:
+            # Write versioned_image values to the file line by line
+            print("Writing Versioned Images to versioned_image_file.txt:")
+            for image_name in extracted_parts:
+                image_tag = formatted_tag
+                versioned_image = f'harbor1.akillisebeke.lab/alix/{image_name}:{image_tag}'
+                tag_image_command = ["docker", "tag", image_name, versioned_image]
+                vf.write(versioned_image + "\n")
+                print(versioned_image)  # Print for verification
 
-                    # Tag the Docker image
-                    subprocess.run(tag_image_command)
+                # Tag the Docker image
+                subprocess.run(tag_image_command)
 
-                    # Push the tagged image to a Docker registry
-                    push_image_command = ["docker", "push", versioned_image]
-                    subprocess.run(push_image_command)
+                # Push the tagged image to a Docker registry
+                push_image_command = ["docker", "push", versioned_image]
+                subprocess.run(push_image_command)
 
-            print("Updated Tag Value:")
-            print(formatted_tag)
+        print("Updated Tag Value:")
+        print(formatted_tag)
+        print(extracted_parts)
 
-        except FileNotFoundError:
-        print(f"The file '{docker_file_name}' or '{version}' was not found.")
+    except FileNotFoundError:
+        print(f"The file '{docker_file_name}' or 'version.txt' was not found.")
     except IOError as e:
         print(f"An error occurred while reading or writing the file: {e}")
